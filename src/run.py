2ed1486636eb487d52abf02f4d372ed5536fe7c3
Pedro <pedrogas_g@hotmail.com>
2021-03-16 22:47:31 +0100

    Changing to DqnAgent

--- src/run.py
+++ src/run.py
@@ -9,7 +9,8 @@ import tensorflow as tf
 
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.initializers import VarianceScaling
+from tensorflow.keras.activations import relu
+from tensorflow.keras.initializers import VarianceScaling, RandomUniform, Constant
 
 
 
@@ -23,11 +24,20 @@ from tf_agents.policies import random_tf_policy
 
 from tf_agents.environments import tf_py_environment
 
-from tf_agents.networks.actor_distribution_network import ActorDistributionNetwork
+from tf_agents.agents.dqn.dqn_agent import DqnAgent
 
-from tf_agents.agents.reinforce.reinforce_agent import ReinforceAgent
+from tf_agents.policies.random_tf_policy import RandomTFPolicy
 
 from tf_agents.replay_buffers.tf_uniform_replay_buffer import TFUniformReplayBuffer
+
+from tf_agents.drivers.dynamic_episode_driver import DynamicEpisodeDriver
+
+from tf_agents.train.utils import strategy_utils
+
+from tf_agents.specs import tensor_spec
+
+from tf_agents.networks.sequential import Sequential
+
 
 if __name__ == '__main__':
     
@@ -36,23 +46,44 @@ if __name__ == '__main__':
     env = tf_py_environment.TFPyEnvironment(py_env)
 
 
-    fc_layer_params = (100,)
+    fc_layer_params = [100,50]
     learning_rate = 1e-3
     episodes_count = 10
-    num_iterations = 250
+    num_iterations = 20000
     replay_buffer_capacity = 2000
     collect_episodes_per_iteration = 2
+    initial_collect_steps = 100
+
+    log_interval = 200
+    eval_interval = 1000
+
+    action_tensor_spec = tensor_spec.from_spec(env.action_spec())
+    num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
     
-    log_interval = 25
-    eval_interval = 50
-    
-    actor = ActorDistributionNetwork(env.observation_spec(), env.action_spec(), fc_layer_params=fc_layer_params)
-    
-    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate)
+    strategy = strategy_utils.get_strategy(tpu=False, use_gpu=True)
+
+    dense_layers = [Dense(
+        num_units,
+        activation=relu,
+        kernel_initializer=VarianceScaling(
+            scale=2.0,
+            mode='fan_in',
+            distribution='truncated_normal')) for num_units in fc_layer_params]
+
+    q_values_layer = Dense(
+        num_actions,
+        activation=None,
+        kernel_initializer=RandomUniform(minval=-0.03, maxval=0.03),
+        bias_initializer=Constant(-0.2))
+
+    q_net = Sequential(dense_layers + [q_values_layer])
+
+     
+    optimizer = Adam(learning_rate=learning_rate)
 
     train_step_counter = tf.Variable(0)
 
-    agent = ReinforceAgent(env.time_step_spec(), env.action_spec(), actor_network=actor, optimizer=optimizer, normalize_returns=True, train_step_counter=train_step_counter)
+    agent = DqnAgent(env.time_step_spec(), env.action_spec(), q_network=q_net, optimizer=optimizer, td_errors_loss_fn=common.element_wise_squared_loss, train_step_counter=train_step_counter)
 
     policy = agent.policy
     
@@ -60,30 +91,39 @@ if __name__ == '__main__':
 
     agent.train = common.function(agent.train)
 
-    def collect_episode(environment, policy, num_episodes):
-
-        episode_counter = 0
-        environment.reset()
-
-        while episode_counter < num_episodes:
-            time_step = environment.current_time_step()
-            action_step = policy.action(time_step)
-            next_time_step = environment.step(action_step.action)
-            traj = trajectory.from_transition(time_step, action_step, next_time_step)
-
-            # Add trajectory to the replay buffer
-            replay_buffer.add_batch(traj)
-
-            if traj.is_boundary():
-                episode_counter += 1
-
     agent.train_step_counter.assign(0)
 
-    compute_avg_return(env, policy, episodes_count)
+    replay_observer = [replay_buffer.add_batch]
+    def collect_step(environment, policy, buffer):
+        time_step = environment.current_time_step()
+        action_step = policy.action(time_step)
+        next_time_step = environment.step(action_step.action)
+        traj = trajectory.from_transition(time_step, action_step, next_time_step)
 
+        # Add trajectory to the replay buffer
+        buffer.add_batch(traj)
+
+    def collect_data(env, policy, buffer, steps):
+        for _ in range(steps):
+            collect_step(env, policy, buffer)
+
+    #driver = DynamicEpisodeDriver(env, policy, replay_observer, num_episodes=collect_episodes_per_iteration)
+    random_policy = RandomTFPolicy(env.time_step_spec(), env.action_spec())
+    collect_data(env, random_policy, replay_buffer, 100)
+    
+    dataset = replay_buffer.as_dataset(
+        num_parallel_calls=3, 
+        sample_batch_size=env.batch_size, 
+        num_steps=2).prefetch(3)
+
+    iterator = iter(dataset)
+    collect_data(env, agent.collect_policy, replay_buffer, 2)
+    #final_time_step, policy_state = driver.run()
+    
     for _ in range(num_iterations):
-        collect_episode(env, policy, collect_episodes_per_iteration)
-        experience = replay_buffer.gather_all()
+        #final_time_step, policy_state = driver.run(final_time_step, policy_state)
+        collect_data(env, agent.collect_policy, replay_buffer, 2)
+        experience, _ = next(iterator)
         train_loss = agent.train(experience)
         replay_buffer.clear()
         step = agent.train_step_counter.numpy()
@@ -93,5 +133,7 @@ if __name__ == '__main__':
 
         if step % eval_interval == 0:
             compute_avg_return(env, policy, episodes_count)
+    
 
-    #TODO    replay_observer = [replay_buffer.add_batch]
+
+    #TODO    
