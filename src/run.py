8ebaf17f81ea04e955ab211bf4d91c310442ef90
Pedro <pedrogas_g@hotmail.com>
2021-03-19 02:51:44 +0100

    Using tf agents lerner

--- src/run.py
+++ src/run.py
@@ -12,17 +12,15 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.activations import relu
 from tensorflow.keras.initializers import VarianceScaling, RandomUniform, Constant
 
-
-
-from tf_agents.environments import suite_pybullet, suite_gym
+from tf_agents.environments import suite_gym
 
 from tf_agents.utils import common
 
 from tf_agents.trajectories import trajectory
 
-from tf_agents.policies import random_tf_policy
-
 from tf_agents.environments import tf_py_environment
+
+from tf_agents.train.learner import Learner
 
 from tf_agents.agents.dqn.dqn_agent import DqnAgent
 
@@ -34,10 +32,11 @@ from tf_agents.drivers.dynamic_episode_driver import DynamicEpisodeDriver
 
 from tf_agents.train.utils import strategy_utils
 
+from tf_agents.train.utils import train_utils
+
 from tf_agents.specs import tensor_spec
 
 from tf_agents.networks.sequential import Sequential
-
 
 if __name__ == '__main__':
     
@@ -61,40 +60,34 @@ if __name__ == '__main__':
     num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
     
     strategy = strategy_utils.get_strategy(tpu=False, use_gpu=True)
-    
-    dense_layers = [Dense(
-        num_units,
-        activation=relu,
-        kernel_initializer=VarianceScaling(
-            scale=2.0,
-            mode='fan_in',
-            distribution='truncated_normal')) for num_units in fc_layer_params]
-
-    q_values_layer = Dense(
-        num_actions,
-        activation=None,
-        kernel_initializer=RandomUniform(minval=-0.03, maxval=0.03),
-        bias_initializer=Constant(-0.2))
-
-    q_net = Sequential(dense_layers + [q_values_layer])
-
-    
-    optimizer = Adam(learning_rate=learning_rate)
-
-    train_step_counter = tf.Variable(0)
-
+        
     with strategy.scope():
+        dense_layers = [Dense(
+            num_units,
+            activation=relu,
+            kernel_initializer=VarianceScaling(
+                scale=2.0,
+                mode='fan_in',
+                distribution='truncated_normal')) for num_units in fc_layer_params]
+
+        q_values_layer = Dense(
+            num_actions,
+            activation=None,
+            kernel_initializer=RandomUniform(minval=-0.03, maxval=0.03),
+            bias_initializer=Constant(-0.2))
+
+        q_net = Sequential(dense_layers + [q_values_layer])
+        optimizer = Adam(learning_rate=learning_rate)
+        train_step_counter = train_utils.create_train_step()        
         agent = DqnAgent(env.time_step_spec(), env.action_spec(), q_network=q_net, optimizer=optimizer, td_errors_loss_fn=common.element_wise_squared_loss, train_step_counter=train_step_counter)
+    
 
     policy = agent.policy
-    
+        
     replay_buffer = TFUniformReplayBuffer(data_spec=agent.collect_data_spec, batch_size=env.batch_size, max_length=replay_buffer_capacity)
     
-    
-    agent.train = common.function(agent.train)
-
     agent.train_step_counter.assign(0)
-    
+        
     def collect_step(environment, policy, buffer):
         time_step = environment.current_time_step()
         action_step = policy.action(time_step)
@@ -109,40 +102,43 @@ if __name__ == '__main__':
             collect_step(env, policy, buffer)
 
     #driver = DynamicEpisodeDriver(env, policy, replay_observer, num_episodes=collect_episodes_per_iteration)
-
+    
     random_policy = RandomTFPolicy(env.time_step_spec(), env.action_spec())
 
     collect_data(env, random_policy, replay_buffer, 100)
 
-    dataset = replay_buffer.as_dataset(
-        num_parallel_calls=3, 
-        sample_batch_size=env.batch_size, 
-        num_steps=2).prefetch(3)
+    def experience_fn():
+        with strategy.scope():
+            return replay_buffer.as_dataset(
+                                            num_parallel_calls=3, 
+                                            sample_batch_size=env.batch_size, 
+                                            num_steps=2).prefetch(3)
 
-    iterator = iter(dataset)
-
+    learner = Learner(
+                '/tmp/models',
+                train_step_counter,
+                agent,
+                experience_fn,
+                strategy=strategy)
+                
     collect_data(env, agent.collect_policy, replay_buffer, 2)
-    #final_time_step, policy_state = driver.run()
-
-    def _reduce_loss(loss):
-      return strategy.reduce(tf.distribute.ReduceOp.SUM, loss, axis=None)
-    
+        #final_time_step, policy_state = driver.run()
+        
     for _ in range(num_iterations):
         #final_time_step, policy_state = driver.run(final_time_step, policy_state)
 
         collect_data(env, agent.collect_policy, replay_buffer, 2)
-        experience, _ = next(iterator)
-        loss_info = strategy.run(agent.train, args=(experience,))
-        train_loss = tf.nest.map_structure(_reduce_loss, loss_info)
+
+        train_loss = learner.run(iterations=1)
         replay_buffer.clear()
-        step = agent.train_step_counter.numpy()
+        step = learner.train_step_numpy
 
         if step % log_interval == 0:
             print('step = {0}: loss = {1}'.format(step, train_loss.loss))
 
         if step % eval_interval == 0:
             compute_avg_return(env, policy, episodes_count)
-    
+        
 
 
     #TODO    
